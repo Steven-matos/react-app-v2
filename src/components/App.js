89a475fdbd1fea816a2c3c45920badc2eeb7e40d
Steven <38477361+Steven-matos@users.noreply.github.com>
2019-04-13 01:07:44 -0400

    React-app-v2 initial

---
+++ src/components/App.js
@@ -0,0 +1,105 @@
+import React from 'react';
+
+const Header = (props) => {
+  return (
+    <header>
+      <h1>{ props.title }</h1>
+      <span className="stats">Player:{ props.totalPlayer }</span>
+    </header>
+  );
+}
+
+const Player = (props) => {
+  return (
+    <div className="player">
+      <span className="player-name">
+      <button className="remove-player" onClick={ () => props.removePlayer(props.id) }>✖</button>
+      { props.name }
+      </span>
+      <Counter />
+    </div>
+  );
+}
+
+class Counter extends React.Component {
+  state= {
+    score: 0
+  }
+
+  incrementScore = () =>{
+    this.setState( prevState => ({
+        score: prevState.score + 1
+    }));
+  }
+
+  decrementScore = () =>{
+    this.setState( prevState => ({
+        score: prevState.score - 1
+    }));
+  }
+
+  render(){
+    return (
+      <div className="counter">
+        <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
+        <span className="counter-score">{ this.state.score }</span>
+        <button className="counter-action increment" onClick={this.incrementScore}> + </button>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+
+  state = {
+    players: [
+      {
+        name: "Steven",
+        id: 1
+      },
+      {
+        name: "Ana",
+        id:2
+      },
+      {
+        name: "Anthony",
+        id: 3
+      },
+      {
+        name: "Anny",
+        id: 4
+      }
+    ]
+  };
+
+  handleRemovePlayer = (id) => {
+    this.setState( prevState => {
+      return {
+        players: prevState.players.filter(p => p.id !== id )
+      }
+    });
+  }
+
+  render(){
+    return (
+      <div className="scoreboard">
+        <Header
+          title="Scoreboard"
+          totalPlayer={this.state.players.length}
+        />
+
+      {/*player list */}
+      {this.state.players.map( player =>
+        <Player
+        name={player.name}
+        id={player.id}
+        key={player.id.toString()}
+        removePlayer={this.handleRemovePlayer}
+        />
+      )}
+      </div>
+    );
+  }
+}
+
+export default App;
