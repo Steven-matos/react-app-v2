39a0c05dac97e1eb7107d95ddf9144e72a501330
Steven <38477361+Steven-matos@users.noreply.github.com>
2019-04-13 22:22:08 -0400

    Player and Counter modules

---
+++ src/components/Counter.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+
+  incrementScore = () =>{
+    this.setState( prevState => ({
+        score: prevState.score + 1
+    }));
+  }
+
+  decrementScore = () =>{
+    this.setState( prevState => ({
+        score: prevState.score - 1
+    }));
+  }
+
+  render(){
+    return (
+      <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <span className="counter-score">{ this.props.score }</span>
+        <button className="counter-action increment"> + </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
