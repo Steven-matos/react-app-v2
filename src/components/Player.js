73186ae3e771c7c6aebd65fccdb3d6997baf3ab9
Steven <38477361+Steven-matos@users.noreply.github.com>
2019-04-23 01:08:28 -0400

    adding high score

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,7 +1,19 @@
 import React, { PureComponent } from 'react';
+import PropTypes from "prop-types"
 import Counter from './Counter.js';
+import Icon from './Icon.js';
 
 class Player extends PureComponent {
+  static propTypes = {
+    name: PropTypes.string,
+    id: PropTypes.number,
+    removePlayer: PropTypes.func,
+    score: PropTypes.number,
+    index: PropTypes.number,
+    changeScore: PropTypes.func,
+    isHighScore: PropTypes.bool
+  }
+
   render() {
 
     const {
@@ -16,13 +28,14 @@ class Player extends PureComponent {
     return (
       <div className="player">
         <span className="player-name">
-        <button className="remove-player" onClick={ () => removePlayer(id) }>✖</button>
-        { name }
+          <button className="remove-player" onClick={ () => removePlayer(id) }>✖</button>
+          <Icon isHighScore={this.props.isHighScore} />
+          { name }
         </span>
         <Counter
-        score={score}
-        index={index}
-        changeScore={changeScore}
+          score={score}
+          index={index}
+          changeScore={changeScore}
         />
       </div>
     );
