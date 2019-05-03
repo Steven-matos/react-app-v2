c056fd9c905eddacc9d0f8698accd39e656675f6
Steven <38477361+Steven-matos@users.noreply.github.com>
2019-05-03 18:15:14 -0400

    Updates

---
+++ precache-manifest.b67054c065a4cbc66957b10e59e6f58b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c7117bc11c0392666938fcea8ca15e6",
+    "url": "/ScoreBoard-app/index.html"
+  },
+  {
+    "revision": "44bab19b0a0704dcc3c4",
+    "url": "/ScoreBoard-app/static/css/main.140df8a8.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/ScoreBoard-app/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "44bab19b0a0704dcc3c4",
+    "url": "/ScoreBoard-app/static/js/main.52972234.chunk.js"
+  },
+  {
+    "revision": "f1e4659d56de7b3fe19f",
+    "url": "/ScoreBoard-app/static/js/runtime~main.dacbde15.js"
+  }
+]);
